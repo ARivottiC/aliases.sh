5e3be1ddcaa43f6789edae619ec175cb384bbfc3
André Rivotti Casimiro <andre@convergent-evolution.com>
2017-01-27 14:42:43 +0000

    improved $HOME/bin script handling

--- generic/PATH-HOME-bin.sh
+++ generic/PATH-HOME-bin.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 # prepends $HOME/bin to $PATH
-[ -d ${HOME}/bin ] && PATH=${HOME}/bin:${PATH}
+if [ -d ${HOME}/bin ]; then
+    PATH=${HOME}/bin:${PATH}
+
+    for x in $(find ${HOME}/.bash_aliases.d -type f -path "*/bin/*" -perm -555 ); do
+        source_file=$x
+        target_file=$(basename $x)
+
+        echo $source_file
+        echo $target_file
+        cmp --silent $source_file $target_file || cp $source_file ${HOME}/bin/$target_file
+    done
+fi

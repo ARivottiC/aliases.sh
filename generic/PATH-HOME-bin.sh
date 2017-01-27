ca360c2d0f8d01f9e47eec2bf6e3472ef8a43077
André Rivotti Casimiro <andre@convergent-evolution.com>
2017-01-27 15:01:08 +0000

    fixed bin scripts detection

--- generic/PATH-HOME-bin.sh
+++ generic/PATH-HOME-bin.sh
@@ -5,10 +5,7 @@ if [ -d ${HOME}/bin ]; then
 
     for x in $(find ${HOME}/.bash_aliases.d -type f -path "*/bin/*" -perm -555 ); do
         source_file=$x
-        target_file=$(basename $x)
-
-        echo $source_file
-        echo $target_file
-        cmp --silent $source_file $target_file || cp $source_file ${HOME}/bin/$target_file
+        target_file=${HOME}/bin/$(basename $x)
+        cmp --silent $source_file $target_file || cp $source_file $target_file
     done
 fi

05a0cdd9b598031f296ffe44523fedca620be8b6
André Rivotti Casimiro <andre@convergent-evolution.com>
2017-06-06 15:14:58 +0100

    fixed virtualenv's aliases

--- generic/alias-rmvirtualenv.sh
+++ generic/alias-rmvirtualenv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # assume current dir as env_name if no parameter is given
-alias workon='__fn(){ unset -f __fn; rmvirtualenv  ${@:-$(basename $(pwd))}; };__fn'
+alias rmvirtualenv='__fn(){ unset -f __fn; rmvirtualenv  ${@:-$(basename $(pwd))}; };__fn'

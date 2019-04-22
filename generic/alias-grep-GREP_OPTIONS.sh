d0a5b416eb36a413820f3837ec5a3a2f5529b63f
André Rivotti Casimiro <andre@convergent-evolution.com>
2019-04-22 13:00:10 +0100

    grep: better handling of GREP_OPTIONS support

--- generic/alias-grep-GREP_OPTIONS.sh
+++ generic/alias-grep-GREP_OPTIONS.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # adds $GREP_OPTIONS support for grep
-alias grep="grep $GREP_OPTIONS"
-unset GREP_OPTIONS
+GREP_OPTIONS='--version' grep 2>&1 | GREP_OPTIONS= grep -q 'GREP_OPTIONS is deprecated' 2>&1
+retval=$?
+if [ $retval -eq 0 ]; then
+    alias grep="grep $GREP_OPTIONS"
+    alias egrep="egrep $GREP_OPTIONS"
+    alias fgrep="fgrep $GREP_OPTIONS"
+    unset GREP_OPTIONS
+fi
